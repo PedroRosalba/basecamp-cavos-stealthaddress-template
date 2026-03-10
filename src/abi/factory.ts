0cfe5d7e365a2781005a80882d3e38ac7235db19
PedroRosalba <pedro.dandrea.rpd@gmail.com>
2026-03-09 23:57:23 -0300

    chore: abi fmt

--- src/abi/factory.ts
+++ src/abi/factory.ts
@@ -1,3 +1,12 @@
 export const STEALTH_FACTORY_ABI = [
-    { type: "function", name: "deploy_stealth_account", inputs: [{ name: "stealth_pubkey_x", type: "felt" }, { name: "stealth_pubkey_y", type: "felt" }, { name: "salt", type: "felt" }], outputs: [{ name: "address", type: "felt" }] },
-  ];
+  {
+    type: "function",
+    name: "deploy_stealth_account",
+    inputs: [
+      { name: "stealth_pubkey_x", type: "felt" },
+      { name: "stealth_pubkey_y", type: "felt" },
+      { name: "salt", type: "felt" },
+    ],
+    outputs: [{ name: "address", type: "felt" }],
+  },
+];
